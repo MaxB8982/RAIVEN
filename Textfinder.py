ad804a4a839ece6c6b96fa4eeebb9c143e2c5d19
Saratheelf <76224812+Saratheelf@users.noreply.github.com>
2020-12-30 18:50:02 -0800

    edited some more.

--- Textfinder.py
+++ Textfinder.py
@@ -1,7 +1,8 @@
-import requests
+import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 
 def isolate (url):
-    req = requests.get(url = url, headers = {'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
-    soup = BeautifulSoup(req.content, 'html.parser')
+    req = urllib.request.Request(url = url, headers = {'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'})
+    response = urllib.request.urlopen(req)
+    soup = BeautifulSoup(response.read(), 'html.parser')
     return soup.get_text()
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # https://realpython.com/python-formatted-output/
 import Yearsold
+import Textfinder
 url = input ("Please copy and paste the website URL so that we can check it's credibility: ")
 date_grade = Yearsold.compute(url)
+find_text = Textfinder.isolate(url)
 print("This source got a " + str(date_grade) + " out of 10. Older sources tend to be less reliable since their information may no longer be relevent.")
+print(find_text)
