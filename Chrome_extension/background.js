8f2fc5a7b78071bde25f682d566062e79110a432
Saratheelf <76224812+Saratheelf@users.noreply.github.com>
2021-01-10 20:55:02 -0800

    The server makes requests and will send back data!!! I have alomst finished this part of the prototype.

--- Chrome_extension/background.js
+++ Chrome_extension/background.js
@@ -1,5 +1,22 @@
-chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    // Send request to server with...
-    //sendRequest('http://localhost:8890?url=${tab.url}')
-    console.log("Script exicuted!")
+chrome.browserAction.onClicked.addListener(function (tab) {
+  //Fired when User Clicks ICON
+  console.log("Script exicuted!");
+  const xhr = new XMLHttpRequest(),
+    method = "GET",
+    url = "http://localhost:8080/?url=" + tab.url ;
+
+  xhr.open(method, url, true);
+  xhr.onreadystatechange = function () {
+    // In local files, status is 0 upon success in Mozilla Firefox
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      var status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        // The request has been completed successfully
+        console.log(xhr.responseText);
+      } else {
+        // Oh no! There has been an error with the request!
+      }
+    }
+  };
+  xhr.send();
 });
