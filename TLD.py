cf85be354e9a58fa93f2bdc23faa926cbee053d3
MaxB8982 <76224680+MaxB8982@users.noreply.github.com>
2021-02-02 11:49:02 -0800

    Create TLD.py

---
+++ TLD.py
@@ -0,0 +1,10 @@
+def grade(url):
+  if url.count(".com/"):
+    return 3
+  elif url.count(".org/"):
+    return 5
+  elif url.count(".edu/"):
+    return 8
+  elif url.count(".gov/"):
+    return 10
+# If we want to add more TLDs (Top-level Domains) that can happen, this is just a basic set for now
