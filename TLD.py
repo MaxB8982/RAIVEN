931572b83ae12822c1e2310f92d11961d2dcfbb5
Saratheelf <76224812+Saratheelf@users.noreply.github.com>
2021-02-02 13:34:12 -0800

    added -1 grade for errors, changed function name so it's not confusing

--- TLD.py
+++ TLD.py
@@ -1,4 +1,4 @@
-def grade(url):
+def grade_url(url):
   if url.count(".com/"):
     return 3
   elif url.count(".org/"):
@@ -7,4 +7,6 @@ def grade(url):
     return 8
   elif url.count(".gov/"):
     return 10
+  else:
+    return int(-1)
 # If we want to add more TLDs (Top-level Domains) that can happen, this is just a basic set for now
