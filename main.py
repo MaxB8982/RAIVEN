4c1e744acc49d8a0ece2933d3a94edd866b44434
Saratheelf <76224812+Saratheelf@users.noreply.github.com>
2021-02-04 11:53:16 -0800

    More commenting, and I added TLD.py to the functions main.py calls

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bottle import route, run, request
 import Yearsold
 import Textfinder
 import Blacklist
+import TLD
 
 # add / to localhost/8080
 @route('/')
@@ -14,8 +15,11 @@ def give_main():
     #grade the date the page was published
     date_grade = Yearsold.compute(url)
     #blacklist_grade = Blacklist.grade(url)
+    #change this^^^^^
     #isolate the text of the page from misc. code stuff
     text_content = Textfinder.isolate(url)
+    #grade the URL ending
+    tld_grade = TLD.grade_url(url)
     #final_grade = empty list
     final_grade = []
     #if the grade for the publish date is greater then two...
@@ -23,18 +27,26 @@ def give_main():
         #add to final_grade list
         final_grade.append(date_grade)
         #rturned_date_stuff = string
-        returned_date_stuff = ("This source got a " + str(date_grade) + " out of 10. Older sources tend to be less reliable since their information may no longer be relevent.")
+        returned_date_stuff = ("This source got a " + str(date_grade) + " out of 10 for publish date. Older sources tend to be less reliable since their information may no longer be relevent.")
     #if the date grading function returns an error...
     elif (date_grade == int(-1)):
-        #returned_date_stuff = string
-        returned_date_stuff = ("Sorry, an error occurred.")
+        #returned_date_stuff = error string
+        returned_date_stuff = ("Sorry, an error occurred in the grading process for the publish date.")
     #elif (blacklist_grade >= 2):
         #final_grade.append(blacklist_grade)
-        #returned_blacklist_stuff = (" This source got a " + str(blacklist_grade) + " out of 10. Sources that are biased towards a certain opinion tend to be less reliable.")
+        #returned_blacklist_stuff = (" This source got a " + str(blacklist_grade) + " out of 10 for the amount of bias. Sources that are biased towards a certain opinion tend to be less reliable.")
     #elif (blacklist_grade == int(-1)):
-        #returned_blacklist_stuff = (" Sorry, an error occurred.")
+        #returned_blacklist_stuff = (" Sorry, an error occurred in the grading process for bias.")
+    #if the url ending grade is more or equal to 2...
+    if (tld_grade >= 2):
+        #returned_url_stuff = string
+        returned_url_stuff = (" This source got a " + str(tld_grade) + " out of 10 for it's url ending. Different url endings haev varying degrees of credibility.")
+    #if the bias grading returns and error...
+    elif (tld_grade == int(-1)):
+        #returned_url_stuff = error string
+        returned_url_stuff = ("Sorry, an error occurred in the grading process for the url ending.")
     #send this back to the google extension:
-    return(returned_date_stuff)
+    return(returned_date_stuff + returned_url_stuff)
 
 #this is how the google extension accesses the stuff returned by main.py
 run(host='localhost', port=8080, debug=True)
