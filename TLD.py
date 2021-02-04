1a82ee7258ec5ca7360c62a6f960ae3715beb58c
MaxB8982 <76224680+MaxB8982@users.noreply.github.com>
2021-02-04 11:33:52 -0800

    Updated a bunch of stuff, notes in description
     - Added .blog .co .us .biz .io
     - updated grading again, added new grade for .blog
     - commented reason for each grade, open for change

--- TLD.py
+++ TLD.py
@@ -1,11 +1,18 @@
 def grade_url(url):
-  if url.count(".com/" or ".net/"):
-    return 4
-  elif url.count(".org/"):
-    return 6
+  if url.count(".blog/"):
+    # .blog is here since it is intended for personal blogging, which usually isnt a good source
+    return 2
+  elif url.count(".com/" or ".net/" or ".io/"):
+    # these TLDs are here since they are very generic and general use
+    return 5
+  elif url.count(".org/" or ".biz/"):
+    # These TLDs are generally for businesses and sources, generally trustworthy in some to most scenerios
+    return 8
   elif url.count(".edu/"):
+    # .edu is pretty trustworthy, its also probably a bad idea to put the education TLD low for a school project haha
     return 9
-  elif url.count(".gov/"):
+  elif url.count(".gov/" or ".co/" or ".us/"):
+    # This decision might be controversial, but I would say the government TLds are very trustworthy, though there may be exceptions in the future. Currently though, this is probably the best idea
     return 10
   else:
     return int(-1)
